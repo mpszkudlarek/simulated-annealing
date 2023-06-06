27426ec3281df9465edf97a44db5a710378f3f3a
mszkudla <259248@student.pwr.edu.pl>
2023-06-06 13:42:25 +0200

    add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simulated Annealing
+
+This is a Python implementation of the Simulated Annealing algorithm. It allows you to find the optimal solution for an optimization problem. The cost function represents the objective function to be optimized.
--- sa.py
+++ sa.py
@@ -14,36 +14,32 @@ def accept_probability(curr_energy, new_energy, temp):
 
 
 def simulated_annealing(initial_solution, initial_temperature, num_iterations, cool_rate):
-    current_solution = initial_solution
-    c_max = current_solution
-    best_c_max = current_solution
+    curr_solution = initial_solution
+    best_solution = curr_solution
 
-    current_energy = cost_function(current_solution)
-    pi = current_energy
+    current_energy = cost_function(curr_solution)
+    best_energy = current_energy
 
     temp = initial_temperature
 
     for iteration in range(num_iterations):
-        new_solution = current_solution + random.uniform(-1, 1)
+        new_solution = curr_solution + random.uniform(-1, 1)
         new_energy = cost_function(new_solution)
 
         if accept_probability(current_energy, new_energy, temp) > random.uniform(0, 1):
-            current_solution = new_solution
+            curr_solution = new_solution
             current_energy = new_energy
 
-        if new_energy < pi:
-            c_max = new_solution
-            pi = new_energy
+        if new_energy < best_energy:
+            curr_solution = new_solution
+            current_energy = new_energy
 
-        if new_energy < cost_function(best_c_max):
-            best_c_max = new_solution
+        if new_energy < best_energy:
+            best_solution = new_solution
+            best_energy = new_energy
 
         temp *= cool_rate
-
-    if cost_function(c_max) < cost_function(best_c_max):
-        return c_max, pi
-    else:
-        return best_c_max, pi
+    return best_energy, best_solution
 
 
 def get_user_input():
@@ -54,17 +50,17 @@ def get_user_input():
     return user_solution, user_temp, user_iter, user_cooling_rate
 
 
-def print_results(c_max, pi, measurement_time):
+def print_results(best_solution, best_energy, measurement_time):
     print('\n--------------------Results:--------------------\n')
-    print("c_max:", c_max)
-    print("pi:", pi)
+    print("Best solution:", best_solution)
+    print("Best energy:", best_energy)
     print("Execution time:", measurement_time, "seconds")
 
 
 solution, temperature, iterations, cooling_rate = get_user_input()
 
 start_time = time.time()
-final_c_max, final_pi = simulated_annealing(solution, temperature, iterations, cooling_rate)
+final_solution, final_energy = simulated_annealing(solution, temperature, iterations, cooling_rate)
 execution_time = time.time() - start_time
 
-print_results(final_c_max, final_pi, execution_time)
+print_results(final_solution, final_energy, execution_time)
